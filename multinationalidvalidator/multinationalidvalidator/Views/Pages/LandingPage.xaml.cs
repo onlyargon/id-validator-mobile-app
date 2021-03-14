0223dc84b796cbe7123f63550c3a5be49d86f138
Aruna Wickramasinghe <argon@Arunas-MacBook-Pro.local>
2021-03-14 23:47:14 +0530

    added basic pages and views

---
+++ multinationalidvalidator/multinationalidvalidator/Views/Pages/LandingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace multinationalidvalidator.Views.Pages
+{
+    public partial class LandingPage : ContentPage
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ multinationalidvalidator/multinationalidvalidator/Views/Views/HomeView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace multinationalidvalidator.Views.Views
+{
+    public partial class HomeView : ContentView
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
