9d9f2f1a4a96e2969ceda99272e4775522299691
Aruna Wickramasinghe <argon@Arunas-MacBook-Pro.local>
2021-03-17 23:03:33 +0530

    added country list

---
+++ multinationalidvalidator/multinationalidvalidator/Models/Country/Country.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace multinationalidvalidator.Models.Country
+{
+    public class Currency
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class Language
+    {
+        public string iso639_1 { get; set; }
+        public string iso639_2 { get; set; }
+        public string name { get; set; }
+        public string nativeName { get; set; }
+    }
+
+    public class Translations
+    {
+        public string de { get; set; }
+        public string es { get; set; }
+        public string fr { get; set; }
+        public string ja { get; set; }
+        public string it { get; set; }
+        public string br { get; set; }
+        public string pt { get; set; }
+        public string nl { get; set; }
+        public string hr { get; set; }
+        public string fa { get; set; }
+    }
+
+    public class RegionalBloc
+    {
+        public RegionalBloc()
+        {
+            otherAcronyms = new List<string>();
+            otherNames = new List<string>();
+        }
+
+        public string acronym { get; set; }
+        public string name { get; set; }
+        public List<string> otherAcronyms { get; set; }
+        public List<string> otherNames { get; set; }
+    }
+
+    public class Country
+    {
+        public string name { get; set; }
+        public List<string> topLevelDomain { get; set; }
+        public string alpha2Code { get; set; }
+        public string alpha3Code { get; set; }
+        public List<string> callingCodes { get; set; }
+        public string capital { get; set; }
+        public List<string> altSpellings { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public int population { get; set; }
+        public List<double> latlng { get; set; }
+        public string demonym { get; set; }
+        public double? area { get; set; }
+        public double? gini { get; set; }
+        public List<string> timezones { get; set; }
+        public List<string> borders { get; set; }
+        public string nativeName { get; set; }
+        public string numericCode { get; set; }
+        public List<Currency> currencies { get; set; }
+        public List<Language> languages { get; set; }
+        public Translations translations { get; set; }
+        public string flag { get; set; }
+        public List<RegionalBloc> regionalBlocs { get; set; }
+        public string cioc { get; set; }
+
+        public Country()
+        {
+            topLevelDomain = new List<string>();
+            callingCodes = new List<string>();
+            altSpellings = new List<string>();
+            latlng = new List<double>();
+            timezones = new List<string>();
+            borders = new List<string>();
+            currencies = new List<Currency>();
+            languages = new List<Language>();
+            translations = new Translations();
+            regionalBlocs = new List<RegionalBloc>();
+        }
+    }
+}
---
+++ multinationalidvalidator/multinationalidvalidator/Service/CommonService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using multinationalidvalidator.Models.Country;
+using Newtonsoft.Json;
+
+namespace multinationalidvalidator.Service
+{
+    public class CommonService
+    {
+        public List<Country> GetCountries()
+        {
+            var assembly = typeof(CommonService).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                if (res.Contains("CountryList.json"))
+                {
+                    Stream stream = assembly.GetManifestResourceStream(res);
+
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string json = reader.ReadToEnd();
+                        return JsonConvert.DeserializeObject<List<Country>>(json);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
